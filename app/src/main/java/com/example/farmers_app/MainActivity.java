34bdb2e2053d62d18dda7e9a2a9859cd33a36004
vignesh-0013 <tvmvignesh2006@gmail.com>
2024-08-26 22:15:20 +0530

    include buy option

--- app/src/main/java/com/example/farmers_app/MainActivity.java
+++ app/src/main/java/com/example/farmers_app/MainActivity.java
@@ -12,16 +12,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Find the CardView by ID
         CardView cardView1 = findViewById(R.id.cardView1); // Assuming you have a CardView with id cardView1
-
-        // Set OnClickListener for the CardView
         cardView1.setOnClickListener(v -> {
-            // Create an Intent to navigate to HelloWorldActivity
             Intent intent = new Intent(MainActivity.this,buyactivity.class);
             startActivity(intent);
         });
 
+        CardView cardView2 = findViewById(R.id.cardView2); // Assuming you have a CardView with id cardView2
+        cardView1.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this,buyactivity.class);
+            startActivity(intent);
+        });
         // Repeat the above code for other card views if needed
     }
 }
